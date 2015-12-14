cf6c7a4f6ca77be9c64f59c15946e3c66ead5f4b
Zoltan Laki <lakizoli@yahoo.com>
2015-12-14 13:29:31 +0100

    Reklam libek berakasa

--- ballfall.android/Resources/Resource.Designer.cs
+++ ballfall.android/Resources/Resource.Designer.cs
@@ -31,6 +31,24 @@ namespace ballfall.android
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010003
+			public const int adSize = 2130771971;
+			
+			// aapt resource value: 0x7f010004
+			public const int adSizes = 2130771972;
+			
+			// aapt resource value: 0x7f010005
+			public const int adUnitId = 2130771973;
+			
+			// aapt resource value: 0x7f010002
+			public const int circleCrop = 2130771970;
+			
+			// aapt resource value: 0x7f010001
+			public const int imageAspectRatio = 2130771969;
+			
+			// aapt resource value: 0x7f010000
+			public const int imageAspectRatioAdjust = 2130771968;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -41,11 +59,66 @@ namespace ballfall.android
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050009
+			public const int common_action_bar_splitter = 2131034121;
+			
+			// aapt resource value: 0x7f050000
+			public const int common_signin_btn_dark_text_default = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int common_signin_btn_dark_text_disabled = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int common_signin_btn_dark_text_focused = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int common_signin_btn_dark_text_pressed = 2131034113;
+			
+			// aapt resource value: 0x7f050008
+			public const int common_signin_btn_default_background = 2131034120;
+			
+			// aapt resource value: 0x7f050004
+			public const int common_signin_btn_light_text_default = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int common_signin_btn_light_text_disabled = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int common_signin_btn_light_text_focused = 2131034119;
+			
+			// aapt resource value: 0x7f050005
+			public const int common_signin_btn_light_text_pressed = 2131034117;
+			
+			// aapt resource value: 0x7f05000a
+			public const int common_signin_btn_text_dark = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int common_signin_btn_text_light = 2131034123;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int common_full_open_on_phone = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int common_ic_googleplayservices = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
 			
 			static Drawable()
 			{
@@ -60,8 +133,17 @@ namespace ballfall.android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int main_frame = 2131034112;
+			// aapt resource value: 0x7f080000
+			public const int adjust_height = 2131230720;
+			
+			// aapt resource value: 0x7f080001
+			public const int adjust_width = 2131230721;
+			
+			// aapt resource value: 0x7f080003
+			public const int main_frame = 2131230723;
+			
+			// aapt resource value: 0x7f080002
+			public const int none = 2131230722;
 			
 			static Id()
 			{
@@ -69,6 +151,22 @@ namespace ballfall.android
 			}
 			
 			private Id()
+			{
+			}
+		}
+		
+		public partial class Integer
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int google_play_services_version = 2131099648;
+			
+			static Integer()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Integer()
 			{
 			}
 		}
@@ -92,11 +190,125 @@ namespace ballfall.android
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f040027
+			public const int ApplicationName = 2130968615;
+			
+			// aapt resource value: 0x7f040026
+			public const int Hello = 2130968614;
+			
+			// aapt resource value: 0x7f040022
+			public const int accept = 2130968610;
+			
 			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			public const int auth_google_play_services_client_facebook_display_name = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int auth_google_play_services_client_google_display_name = 2130968576;
+			
+			// aapt resource value: 0x7f040004
+			public const int common_android_wear_notification_needs_update_text = 2130968580;
+			
+			// aapt resource value: 0x7f040011
+			public const int common_android_wear_update_text = 2130968593;
+			
+			// aapt resource value: 0x7f04000f
+			public const int common_android_wear_update_title = 2130968591;
+			
+			// aapt resource value: 0x7f04001d
+			public const int common_google_play_services_api_unavailable_text = 2130968605;
+			
+			// aapt resource value: 0x7f04000d
+			public const int common_google_play_services_enable_button = 2130968589;
+			
+			// aapt resource value: 0x7f04000c
+			public const int common_google_play_services_enable_text = 2130968588;
+			
+			// aapt resource value: 0x7f04000b
+			public const int common_google_play_services_enable_title = 2130968587;
+			
+			// aapt resource value: 0x7f040006
+			public const int common_google_play_services_error_notification_requested_by_msg = 2130968582;
+			
+			// aapt resource value: 0x7f04000a
+			public const int common_google_play_services_install_button = 2130968586;
+			
+			// aapt resource value: 0x7f040008
+			public const int common_google_play_services_install_text_phone = 2130968584;
+			
+			// aapt resource value: 0x7f040009
+			public const int common_google_play_services_install_text_tablet = 2130968585;
+			
+			// aapt resource value: 0x7f040007
+			public const int common_google_play_services_install_title = 2130968583;
+			
+			// aapt resource value: 0x7f040017
+			public const int common_google_play_services_invalid_account_text = 2130968599;
+			
+			// aapt resource value: 0x7f040016
+			public const int common_google_play_services_invalid_account_title = 2130968598;
+			
+			// aapt resource value: 0x7f040005
+			public const int common_google_play_services_needs_enabling_title = 2130968581;
+			
+			// aapt resource value: 0x7f040015
+			public const int common_google_play_services_network_error_text = 2130968597;
+			
+			// aapt resource value: 0x7f040014
+			public const int common_google_play_services_network_error_title = 2130968596;
+			
+			// aapt resource value: 0x7f040003
+			public const int common_google_play_services_notification_needs_update_title = 2130968579;
+			
+			// aapt resource value: 0x7f040002
+			public const int common_google_play_services_notification_ticker = 2130968578;
+			
+			// aapt resource value: 0x7f04001f
+			public const int common_google_play_services_sign_in_failed_text = 2130968607;
+			
+			// aapt resource value: 0x7f04001e
+			public const int common_google_play_services_sign_in_failed_title = 2130968606;
+			
+			// aapt resource value: 0x7f040018
+			public const int common_google_play_services_unknown_issue = 2130968600;
+			
+			// aapt resource value: 0x7f04001a
+			public const int common_google_play_services_unsupported_text = 2130968602;
+			
+			// aapt resource value: 0x7f040019
+			public const int common_google_play_services_unsupported_title = 2130968601;
+			
+			// aapt resource value: 0x7f04001b
+			public const int common_google_play_services_update_button = 2130968603;
+			
+			// aapt resource value: 0x7f040010
+			public const int common_google_play_services_update_text = 2130968592;
+			
+			// aapt resource value: 0x7f04000e
+			public const int common_google_play_services_update_title = 2130968590;
+			
+			// aapt resource value: 0x7f040013
+			public const int common_google_play_services_updating_text = 2130968595;
+			
+			// aapt resource value: 0x7f040012
+			public const int common_google_play_services_updating_title = 2130968594;
+			
+			// aapt resource value: 0x7f04001c
+			public const int common_open_on_phone = 2130968604;
+			
+			// aapt resource value: 0x7f040025
+			public const int create_calendar_message = 2130968613;
+			
+			// aapt resource value: 0x7f040024
+			public const int create_calendar_title = 2130968612;
+			
+			// aapt resource value: 0x7f040023
+			public const int decline = 2130968611;
+			
+			// aapt resource value: 0x7f040021
+			public const int store_picture_message = 2130968609;
+			
+			// aapt resource value: 0x7f040020
+			public const int store_picture_title = 2130968608;
 			
 			static String()
 			{
@@ -104,6 +316,63 @@ namespace ballfall.android
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f070000
+			public const int Theme_IAPTheme = 2131165184;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] AdsAttrs = new int[] {
+					2130771971,
+					2130771972,
+					2130771973};
+			
+			// aapt resource value: 0
+			public const int AdsAttrs_adSize = 0;
+			
+			// aapt resource value: 1
+			public const int AdsAttrs_adSizes = 1;
+			
+			// aapt resource value: 2
+			public const int AdsAttrs_adUnitId = 2;
+			
+			public static int[] LoadingImageView = new int[] {
+					2130771968,
+					2130771969,
+					2130771970};
+			
+			// aapt resource value: 2
+			public const int LoadingImageView_circleCrop = 2;
+			
+			// aapt resource value: 1
+			public const int LoadingImageView_imageAspectRatio = 1;
+			
+			// aapt resource value: 0
+			public const int LoadingImageView_imageAspectRatioAdjust = 0;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
